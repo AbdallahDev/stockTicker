78c5732d062141d30d6a3cf22984cc7afcf1bcc0
AbdallahDev <abdallah.dev85@gmail.com>
2026-02-05 22:58:10 +0300

    stage

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 import { stocks } from "./stockData.js";
 
 const containerEl = document.getElementById("container");
-let containerDOM = "";
 
-stocks.forEach((stock) => {
-  const stockDOM = `
+renderApp();
+setInterval(renderApp, 5000);
+
+function renderApp() {
+  let dateTimeHtml = renderDateTime();
+  let stockQoutesHtml = renderStockQuotes();
+
+  containerEl.innerHTML = dateTimeHtml + stockQoutesHtml;
+}
+
+function renderDateTime() {
+  const now = new Date();
+
+  const options = {
+    timeZone: "Asia/Amman", // Explicitly set to Jordan
+    day: "numeric",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+
+  const jordanTime = now.toLocaleString("en-JO", options);
+  const dateTimeEl = `<div id="dateTime">${jordanTime}</div>`;
+  return dateTimeEl;
+}
+
+function renderStockQuotes() {
+  let containerDOM = "";
+
+  stocks.forEach((stock) => {
+    const stockDOM = `
     <div class="stockQoute">
         <div class="leftData">
             <img class="logo" src="${stock.logo_url}" alt="${stock.security_name} logo">
@@ -19,6 +49,8 @@ stocks.forEach((stock) => {
         </div>
     </div>
     `;
-  containerDOM += stockDOM;
-});
-containerEl.innerHTML = containerDOM;
+    containerDOM += stockDOM;
+  });
+
+  return containerDOM;
+}
